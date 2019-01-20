9f4a62ddb2204041886cbbcafb10e531620292b4
阿卡琳 <hufan.akarin@Gmail.com>
2019-01-20 23:12:03 +0800

    refactor

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import {
   svg
 } from "https://unpkg.com/@polymer/lit-element@0.6.4/lit-element.js?module";
 import { produce } from "https://cdn.jsdelivr.net/npm/immer@1.9.3/dist/immer.module.min.js";
+import { ifDefined } from "https://unpkg.com/lit-html@0.14.0/directives/if-defined.js?module";
+import { repeat } from "https://unpkg.com/lit-html@0.14.0/directives/repeat.js?module";
 
 document.body.addEventListener(
   "dragover",
@@ -30,12 +32,17 @@ customElements.define(
 
       this.worker.addEventListener("message", e => {
         /**
-         * @type {{id:number, type:"meta"|"image"|"url", data:any}}
+         * @type {{id:number, type:"error"|"data", data:any}}
          */
         const data = e.data;
 
+        if (data.type === "error") {
+          alert(`error:${this.tracks[data.id].file.name} is not ncm file.`);
+          return;
+        }
+
         this.tracks = produce(this.tracks, draft => {
-          draft[data.id][data.type] = data.data;
+          Object.assign(draft[data.id], data.payload);
         });
       });
 
@@ -61,12 +68,12 @@ customElements.define(
 
       this.onUploadInputChange = {
         /**
-         * @param {Event} e 
+         * @param {Event} e
          */
-        handleEvent(e) { 
+        handleEvent(e) {
           this.fileHandler(e.target.files);
         }
-      }
+      };
     }
 
     firstUpdated() {
@@ -86,53 +93,76 @@ customElements.define(
       document.addEventListener("play-track", this.playTrackHandler);
     }
 
-    // disconnectedCallback() {
-    //   super.disconnectedCallback();
+    static get style() {
+      return html`
+        <style>
+          section {
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+            grid-gap: 1rem;
+            padding: 1rem;
+          }
 
-    //   this.worker.terminate();
-    //   document.body.removeEventListener("drop", this.dropHandler, false);
-    //   document.removeEventListener("play-track", this.playTrackHandler);
-    // }
+          .big {
+            font-size: 38px;
+            height: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin: 1rem;
+            border: gray dashed 1rem;
+            border-radius: 1rem;
+          }
+
+          audio {
+            width: 100%;
+            position: fixed;
+            bottom: 0;
+            height: 32px;
+          }
+
+          [hidden] {
+            display: none;
+          }
+        </style>
+      `;
+    }
 
     render() {
       return html`
-            <style>
-              section {
-                display: grid;
-                grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-                grid-gap: 1rem;
-                padding: 1rem;
-              }
-              .big {
-                font-size: 38px;
-                height: 100%;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                margin: 1rem;
-                border: gray dashed 1rem;
-                border-radius: 1rem;
-              }
-              audio {
-                width: 100%;
-                position: fixed;
-                bottom: 0;
-              }
-            </style>
-            
-            ${
-              this.tracks.length
-                ? html`<section>
-                        ${this.tracks.map(
-                          track => html`<ncmc-card .track=${track} />`
-                        )}
-                      </section>`
-                : html`<label for="upload-ncm" class="big">Click here or drag and drop ncm files here</label>`
-            }
-            <input id="upload-ncm" type="file" accept=".ncm" multiple @change=${e => this.fileHandler(e.target.files)} hidden />
-            
-            ${this.src && html`<audio controls src=${this.src}>`}
-    `;
+        ${this.constructor.style}
+
+        <section ?hidden=${this.tracks.length === 0}>
+          ${
+            repeat(
+              this.tracks,
+              track => track.id,
+              track => html`
+                <ncmc-card .track=${track} />
+              `
+            )
+          }
+        </section>
+
+        <label ?hidden=${this.tracks.length !== 0} for="upload-ncm" class="big">
+          Click here or drag and drop ncm files here
+        </label>
+
+        <input
+          id="upload-ncm"
+          type="file"
+          accept=".ncm"
+          multiple
+          @change=${e => this.fileHandler(e.target.files)}
+          hidden
+        />
+
+        <audio
+          src=${ifDefined(this.src)}
+          ?controls=${!!this.src}
+          ?hidden=${!this.src}
+        />
+      `;
     }
   }
 );
@@ -160,28 +190,29 @@ customElements.define(
 
     constructor() {
       super();
-      this.playHandler = () => {
+      this.playHandler = e => {
+        e.preventDefault();
         document.dispatchEvent(
           new CustomEvent("play-track", {
-            detail: { url: this.url }
+            detail: { url: this.track.url }
           })
         );
       };
     }
 
-    get url() {
-      return this.track.url || "#";
-    }
-
     get name() {
-      if (this.track.meta) {
+      if (
+        this.track.meta &&
+        this.track.meta.musicName &&
+        this.track.meta.artist
+      ) {
         return (
           this.track.meta.artist.map(ar => ar[0]).join("/") +
           " - " +
           this.track.meta.musicName
         );
       }
-      return "??? - ???";
+      return this.track.file.name.slice(0, -4);
     }
 
     get album() {
@@ -198,61 +229,77 @@ customElements.define(
       return this.track.file.name;
     }
 
+    get albumPic() {
+      return this.track.meta ? this.track.meta.albumPic : placeHolder;
+    }
+
+    static get style() {
+      return html`
+        <style>
+          :host {
+            width: 200px;
+          }
+
+          .main {
+            height: 200px;
+            width: 200px;
+            background-size: cover;
+          }
+
+          .button-group {
+            height: 100%;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+
+          svg {
+            transition: opacity ease 0.5s;
+            opacity: 0;
+          }
+
+          .button-group:hover svg {
+            opacity: 1;
+          }
+
+          .button-group svg {
+            fill: white;
+            height: 50px;
+            width: 50px;
+          }
+
+          .button-group:hover {
+            transition: background-color ease 0.5s;
+            background-color: #00000080;
+          }
+        </style>
+      `;
+    }
+
     render() {
       if (this.track === undefined) return;
+
       return html`
-      <style>
-        :host {
-          width: 200px;
-        }
+        ${this.constructor.style}
 
-        .main {
-          height: 200px;
-          width: 200px;
-          background-size: cover;
-        }
+        <section class="main" style="background-image:url('${this.albumPic}')">
+          <div class="button-group">
+            <a href="#" @click=${this.playHandler}>${playButton()}</a>
+            <a
+              ?disabled=${!this.track.url}
+              href="${this.track.url}"
+              download="${this.downloadName}"
+              >${downloadButton()}</a
+            >
+          </div>
+        </section>
 
-        .button-group {
-          height: 100%;
-          width: 100%;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-        }
-
-        svg {
-          transition: opacity ease 0.5s;
-          opacity: 0;
-        }
-
-        .button-group:hover svg {
-          opacity: 1;
-        }
-
-        .button-group svg {
-          fill: white;
-          height: 50px;
-          width: 50px;
-        }
-
-        .button-group:hover {
-          transition: background-color ease 0.5s;
-          background-color: #00000080;
-        }
-      </style>
-      <section class="main" style="background-image:url('${this.track.image || placeHolder}')">
-        <div class="button-group">
-          <a href="#" @click=${this.playHandler}>${playButton()}</a>
-          <a disabled=${!this.track.url} href="${this.url}" download="${
-        this.downloadName
-      }">${downloadButton()}</a>
-      </div>
-      </section>
-      <section class="info">
-        <p>${this.name}</p>
-        <p>${this.album}</p>
-      </section>
-`;
+        <section class="info">
+          <p>${this.name}</p>
+          <p>${this.album}</p>
+        </section>
+      `;
     }
   }
 );
--- worker.js
+++ worker.js
@@ -16,6 +16,9 @@ const audio_mime_type = {
   flac: "audio/flac"
 };
 
+const defaultAlbumPic =
+  "https://p4.music.126.net/nSsje95JU5hVylFPzLqWHw==/109951163542280093.jpg";
+
 /**
  * @param {Object} e
  * @param {[{id: number, file: File}]} e.data
@@ -27,82 +30,66 @@ self.onmessage = e => {
     /** @type {ArrayBuffer} */
     let ab = reader.readAsArrayBuffer(data.file);
 
-    const dv = new DataView(ab);
+    const dataview = new DataView(ab);
 
     if (
-      dv.getUint32(0, true) !== 0x4e455443 ||
-      dv.getUint32(4, true) !== 0x4d414446
+      dataview.getUint32(0, true) !== 0x4e455443 ||
+      dataview.getUint32(4, true) !== 0x4d414446
     ) {
       self.postMessage({ id: data.id, type: "error", data: "not ncm file" });
+      return;
     }
 
     let offset = 10;
-    let key_len = dv.getUint32(offset, true);
-    offset += 4;
-    const key_data = new Uint8Array(ab, offset, key_len).map(
-      data => data ^ 0x64
-    );
-    offset += key_len;
 
-    const de_key_data = CryptoJS.AES.decrypt(
-      { ciphertext: CryptoJS.lib.WordArray.create(key_data) },
-      CORE_KEY,
-      {
-        mode: CryptoJS.mode.ECB,
-        padding: CryptoJS.pad.Pkcs7
-      }
-    );
-
-    let de_key_data_u8 = new Uint8Array(de_key_data.sigBytes);
-
-    {
-      const words = de_key_data.words;
-      const sigBytes = de_key_data.sigBytes;
-      for (let i = 0; i < sigBytes; i++) {
-        de_key_data_u8[i] = (words[i >>> 2] >>> (24 - (i % 4) * 8)) & 0xff;
-      }
-
-      de_key_data_u8 = de_key_data_u8.slice(17);
-    }
-
-    let key_box = new Uint8Array(Array.from(Array(256).keys()));
-
-    {
-      const key_len = de_key_data_u8.length;
-
-      let j = 0;
-
-      for (let i = 0; i < 256; i++) {
-        j = (key_box[i] + j + de_key_data_u8[i % key_len]) & 0xff;
-        [key_box[i], key_box[j]] = [key_box[j], key_box[i]];
-      }
-
-      key_box = key_box.map(
-        (item, i, arr) => arr[(item + arr[(item + i) & 0xff]) & 0xff]
+    const key_box = (() => {
+      let key_len = dataview.getUint32(offset, true);
+      offset += 4;
+      let key_data = new Uint8Array(ab, offset, key_len).map(
+        data => data ^ 0x64
       );
-    }
+      offset += key_len;
 
-    const meta_data_len = dv.getUint32(offset, true);
-    offset += 4;
-    const meta_data = new Uint8Array(ab, offset, meta_data_len).map(
-      data => data ^ 0x63
-    );
-    offset += meta_data_len;
+      key_data = CryptoJS.AES.decrypt(
+        { ciphertext: CryptoJS.lib.WordArray.create(key_data) },
+        CORE_KEY,
+        {
+          mode: CryptoJS.mode.ECB,
+          padding: CryptoJS.pad.Pkcs7
+        }
+      );
 
-    const meta_data_decoded = CryptoJS.AES.decrypt(
+      let de_key_data = new Uint8Array(key_data.sigBytes);
+
       {
-        ciphertext: CryptoJS.enc.Base64.parse(
-          CryptoJS.lib.WordArray.create(meta_data.slice(22)).toString(
-            CryptoJS.enc.Utf8
-          )
-        )
-      },
-      META_KEY,
-      {
-        mode: CryptoJS.mode.ECB,
-        padding: CryptoJS.pad.Pkcs7
+        const words = key_data.words;
+        const sigBytes = key_data.sigBytes;
+        for (let i = 0; i < sigBytes; i++) {
+          de_key_data[i] = (words[i >>> 2] >>> (24 - (i % 4) * 8)) & 0xff;
+        }
+
+        de_key_data = de_key_data.slice(17);
       }
-    );
+
+      let key_box = new Uint8Array(Array.from(Array(256).keys()));
+
+      {
+        const key_len = de_key_data.length;
+
+        let j = 0;
+
+        for (let i = 0; i < 256; i++) {
+          j = (key_box[i] + j + de_key_data[i % key_len]) & 0xff;
+          [key_box[i], key_box[j]] = [key_box[j], key_box[i]];
+        }
+
+        key_box = key_box.map(
+          (item, i, arr) => arr[(item + arr[(item + i) & 0xff]) & 0xff]
+        );
+      }
+
+      return key_box;
+    })();
 
     /**
      * @typedef {Object} MusicMetaType
@@ -111,48 +98,48 @@ self.onmessage = e => {
      * @property {[[String, Number]]} artist
      * @property {String} album
      * @property {"flac"|"mp3"} format
+     * @property {String} albumPic
      */
 
-    /** @type {MusicMetaType} */
-    const music_meta = JSON.parse(
-      meta_data_decoded.toString(CryptoJS.enc.Utf8).slice(6)
-    );
+    /** @type {MusicMetaType|undefined} */
+    const music_meta = (() => {
+      const meta_data_len = dataview.getUint32(offset, true);
+      offset += 4;
+      if (meta_data_len === 0) {
+        return {
+          album: "\u26A0\uFE0F meta lost",
+          albumPic: defaultAlbumPic
+        };
+      }
 
-    self.postMessage({ id: data.id, type: "meta", data: music_meta });
+      const meta_data = new Uint8Array(ab, offset, meta_data_len).map(
+        data => data ^ 0x63
+      );
+      offset += meta_data_len;
 
-    offset += 9;
-    const image_size = dv.getUint32(offset, true);
-    offset += 4;
-    // const image = new Uint8Array(ab, offset, image_size);
-    // let image_mime_type = "image/*";
+      const meta_data_decoded = CryptoJS.AES.decrypt(
+        {
+          ciphertext: CryptoJS.enc.Base64.parse(
+            CryptoJS.lib.WordArray.create(meta_data.slice(22)).toString(
+              CryptoJS.enc.Utf8
+            )
+          )
+        },
+        META_KEY,
+        {
+          mode: CryptoJS.mode.ECB,
+          padding: CryptoJS.pad.Pkcs7
+        }
+      );
 
-    // if (
-    //   dv.getUint32(offset, true) === 0x474e5089 &&
-    //   dv.getUint32(offset + 4, true) === 0x0a1a0a0d
-    // ) {
-    //   image_mime_type = "image/png";
-    // } else if (dv.getUint32(offset, true) === 0xe0ffd8ff) {
-    //   image_mime_type = "image/jpg";
-    // } else if (
-    //   dv.getUint16(offset, true) === 0x4947 &&
-    //   dv.getUint8(offset + 2) === 0x46
-    // ) {
-    //   image_mime_type = "image/gif";
-    // }
+      const music_meta = JSON.parse(
+        meta_data_decoded.toString(CryptoJS.enc.Utf8).slice(6)
+      );
+      music_meta.albumPic = music_meta.albumPic.replace("http:", "https:");
+      return music_meta;
+    })();
 
-    if (image_size === 0) {
-      offset = 8367;
-    } else {
-      offset += image_size;
-    }
-    // const image_url = URL.createObjectURL(
-    //   new Blob([image], { type: image_mime_type })
-    // );
-    self.postMessage({
-      id: data.id,
-      type: "image",
-      data: music_meta.albumPic.replace("http:", "https:")
-    });
+    offset += dataview.getUint32(offset + 5, true) + 13;
 
     const original_file = new Uint8Array(ab, offset);
     const original_file_length = original_file.length;
@@ -163,12 +150,29 @@ self.onmessage = e => {
       }
     }
 
+    if (music_meta.format === undefined) {
+      music_meta.format = (() => {
+        const [f, L, a, C] = original_file;
+        if (f === 0x66 && L === 0x4c && a === 0x61 && C === 0x43) {
+          return "flac";
+        }
+        return "mp3";
+      })();
+    }
+
     const music_file = new Blob([original_file], {
       type: audio_mime_type[music_meta.format]
     });
 
     const music_url = URL.createObjectURL(music_file);
 
-    self.postMessage({ id: data.id, type: "url", data: music_url });
+    self.postMessage({
+      id: data.id,
+      type: "data",
+      payload: {
+        meta: music_meta,
+        url: music_url
+      }
+    });
   }
 };
